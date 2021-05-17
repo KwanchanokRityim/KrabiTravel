80d020a0ec76a864490a56d796bf5660ae6498ac
KwanchanokRityim <84337481+KwanchanokRityim@users.noreply.github.com>
2021-05-17 23:37:02 +0700

    Add files via upload

---
+++ check_read.php
@@ -0,0 +1,27 @@
+<?php
+	session_start();
+	$serverName = "127.0.0.1:52105";
+	$userName = "azure";
+	$userPassword = "6#vWHD_$";
+	$dbName = "myDatabase";
+
+	$objCon = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+
+	$strSQL = "SELECT * FROM Reservation WHERE id = '".mysqli_real_escape_string($objCon,$_POST['txtID'])."' 
+	and phone_number = '".mysqli_real_escape_string($objCon,$_POST['txtNum'])."'";
+	$objQuery = mysqli_query($objCon,$strSQL);
+	$objResult = mysqli_fetch_array($objQuery,MYSQLI_ASSOC);
+	if(!$objResult)
+	{
+			echo "id and Phone Number Incorrect!";
+			echo "<br>Go to <a href='read.php'> login page</a><br>";
+	}
+	else
+	{
+			$_SESSION["id"] = $objResult["id"];
+			session_write_close();
+			header("location:read2.php");
+			
+	}
+	mysqli_close($objCon);
+?>
